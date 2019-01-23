7d2a84bcd0900d1d867a43d506c7651363cf4e45
agrosson <agrosson@gmail.com>
2019-01-23 14:34:23 +0100

    First commit : file setup 1.	Suppressions des targets de test : 	a.	CountOnMeTests 	b.	CountOnMeUITests 2.	Création d’un nouveau fichier de test 3.	Ajout @testable import CountOnMe 4.	Ajout ExceptionBreakPoint 5.	Installation SwiftLint

---
+++ CountOnMeTests/CountOnMeTests.swift
@@ -0,0 +1,16 @@
+//
+//  CountOnMeTests.swift
+//  CountOnMeTests
+//
+//  Created by ALEXANDRE GROSSON on 23/01/2019.
+//  Copyright © 2019 Ambroise Collon. All rights reserved.
+//
+
+import XCTest
+@testable import CountOnMe
+
+class CountOnMeTests: XCTestCase {
+    func testFirst(){
+        XCTAssert(true)
+    }
+}
