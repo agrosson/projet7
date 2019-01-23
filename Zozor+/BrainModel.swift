0ea66318e1a4fcabf1a677b53021f954a1907ec2
agrosson <agrosson@gmail.com>
2019-01-23 16:33:07 +0100

    Refactoring code for alerts: 1. Creation of alertNewCalculation(); alertEnterCorrectExpression() and alertIncorrectExpresion()
    Creation of class Brain : model of the application

---
+++ Zozor+/BrainModel.swift
@@ -0,0 +1,15 @@
+//
+//  BrainModel.swift
+//  CountOnMe
+//
+//  Created by ALEXANDRE GROSSON on 23/01/2019.
+//  Copyright © 2019 Ambroise Collon. All rights reserved.
+//
+
+import Foundation
+
+class Brain {
+    var stringNumbers = [String]()
+    var operators = ["+"]
+    init() {}
+}
--- Zozor+/ViewController.swift
+++ Zozor+/ViewController.swift
@@ -12,43 +12,51 @@ class ViewController: UIViewController {
     // MARK: - Properties
     var stringNumbers: [String] = [String()]
     var operators: [String] = ["+"]
-    var index = 0
     var isExpressionCorrect: Bool {
         if let stringNumber = stringNumbers.last {
             if stringNumber.isEmpty {
                 if stringNumbers.count == 1 {
-                    let alertVC = UIAlertController(title: "Zéro!",
-                                                    message: "Démarrez un nouveau calcul !",
-                                                    preferredStyle: .alert)
-                    alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                    self.present(alertVC, animated: true, completion: nil)
+                    alertNewCalculation()
                 } else {
-                    let alertVC = UIAlertController(title: "Zéro!",
-                                                    message: "Entrez une expression correcte !",
-                                                    preferredStyle: .alert)
-                    alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                    self.present(alertVC, animated: true, completion: nil)
+                    alertEnterCorrectExpression()
                 }
                 return false
             }
         }
         return true
     }
-
     var canAddOperator: Bool {
         if let stringNumber = stringNumbers.last {
             if stringNumber.isEmpty {
-                let alertVC = UIAlertController(title: "Zéro!",
-                                                message: "Expression incorrecte !",
-                                                preferredStyle: .alert)
-                alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                self.present(alertVC, animated: true, completion: nil)
+                alertIncorrectExpresion()
                 return false
             }
         }
         return true
     }
 
+    // MARK: - Methods
+    func alertNewCalculation() {
+        let alertVC = UIAlertController(title: "Zéro!",
+                                        message: "Démarrez un nouveau calcul !",
+                                        preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    func alertEnterCorrectExpression() {
+        let alertVC = UIAlertController(title: "Zéro!",
+                                        message: "Entrez une expression correcte !",
+                                        preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    func alertIncorrectExpresion() {
+        let alertVC = UIAlertController(title: "Zéro!",
+                                        message: "Expression incorrecte !",
+                                        preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alertVC, animated: true, completion: nil)
+    }
     // MARK: - Outlets
 
     @IBOutlet weak var textView: UITextView!
@@ -130,6 +138,5 @@ class ViewController: UIViewController {
     func clear() {
         stringNumbers = [String()]
         operators = ["+"]
-        index = 0
     }
 }
